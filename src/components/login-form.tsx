41b356286cd8958aff496dd5a20f5c8753d2959a
Jatin <jatin70305@gmail.com>
2024-10-06 11:07:10 +0530

    new changes

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -1,4 +1,6 @@
-import { Button, Card, Stack, TextField } from "@mui/material";
+
+import { Facebook, Google } from "@mui/icons-material";
+import { Button, Typography, Card, Link, Stack, TextField } from "@mui/material";
 import { useState } from "react";
 
 export default function LoginForm() {
@@ -14,28 +16,76 @@ export default function LoginForm() {
     console.log({ email, password });
   };
 
-  return <Card sx={{
+  return <Card elevation={5} sx={{
     margin: 2,
     padding: 2,
+    height: 460,
+    width: 400,
+
   }}>
     <Stack direction="column" spacing={1}>
+      <Typography variant="h4" sx={{
+        textAlign: "center",
+        margin: 20
+      }}>Sign In</Typography>
 
-      <TextField type="email" placeholder="Email" fullWidth onChange={(event) => {
+      <Typography variant="h6">Username</Typography>
+
+      <TextField type="email" placeholder="Email or username" fullWidth onChange={(event) => {
         const newEmail = event.target.value;
         setEmail(newEmail);
       }} />
 
+      <Typography variant="h6" sx={{
+        fontSize: 18
+      }}>Password  <Link sx={{
+        fontWeight: 200,
+        fontSize: 14,
+        cursor: "pointer"
+      }}>forgotten Password?
+        </Link>
+      </Typography>
+
       <TextField type="password" placeholder="Password" fullWidth onChange={handlePasswordChange} />
 
-      <Stack direction="row" justifyContent="space-between">
-        <Button variant="contained" onClick={login}>
-          Login
+      <Stack direction={"column"} >
+
+        <Button variant="contained" fullWidth onClick={login}>
+          Sign In
         </Button>
-        <Button variant="text">
-          Sign Up
-        </Button>
+
+        <Typography sx={{
+          display: "grid",
+          justifyContent: "center",
+          marginTop: 2
+        }}>OR</Typography>
+
+        <Stack direction={"row"} justifyContent={"space-evenly"}
+          sx={{
+            marginTop: 1,
+          }} >
+          <Button variant="outlined" >
+            Sign in with <Facebook color="primary"></Facebook>
+          </Button>
+
+          <Button variant="outlined" >
+            Sign in with   <Google color="primary"></Google>
+          </Button>
+
+        </Stack>
+
+        <p style={{
+          display: "flex",
+          justifyContent: "center",
+        }}> Don't have an account? <Link color="primary" sx={{
+          cursor: "pointer",
+        }}>
+            Sign Up
+          </Link>
+        </p>
+
       </Stack>
 
     </Stack>
-  </Card>
+  </Card >
 }
